cbf2f65ad37e6cbe5e0c19ee9251603867216272
Henry Ramirez <h.ramirez0427@gmail.com>
2015-01-26 10:39:46 -0800

    commit 7
    doing stuff

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -11,10 +11,15 @@
        		return(new me.Rect(0, 0, 64, 64)).toPolygon();
        	}
        }]);
+
+       this.body.setVelocity(5,0);
 	},
 
 	update: function(){
+       if(me.input.isKeyPressed("right")) {
 
+        this.body.vel.x += this.body.accel.x * me.timer.tick;
+       }
 	}
 });
 /**/
