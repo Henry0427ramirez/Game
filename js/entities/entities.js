753ff0f347572041c8216669e58ac942eecec1b7
Henry Ramirez <h.ramirez0427@gmail.com>
2015-02-10 08:40:51 -0800

    commit 24
    adding movement to my enemy creep

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -227,8 +227,17 @@ game.EnemyCreep = me.Entity.extend ({
     	this.renderable.addAnimation("walk", [3, 4, 5], 80);
     	this.renderable.setCurrentAnimation("walk");
     },
-    update: function(){
 
+    update: function(delta){
+    //makes the enemy move.
+     this.body.vel.x -= this.body.accel.x * me.timer.tick;
+
+
+    this.body.update(delta);
+
+   
+    this._super(me.Entity,"update", [delta]);
+   
     return true;
     }
 });
