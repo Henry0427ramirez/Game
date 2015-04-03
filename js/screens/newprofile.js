94883a63d94498cd82e384918b43ab0e4d90db87
Henry Ramirez <h.ramirez0427@gmail.com>
2015-04-03 08:57:01 -0700

    commit 62
    finished video  53

--- js/screens/newprofile.js
+++ js/screens/newprofile.js
@@ -4,6 +4,8 @@ game.NewProfile = me.ScreenObject.extend({
 	 */
 	onResetEvent: function() {	
 		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('new-screen')), -10); // TODO
+        document.getElementByld("input").style.visibility = "visible";
+        document.getElementByld("register").style.visibility = "visible"; 
 
         me.input.unbindKey(me.input.KEY.B);
         me.input.unbindKey(me.input.KEY.Q);
@@ -34,6 +36,7 @@ game.NewProfile = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
- 
+       document.getElementByld("input").style.visibility = "visible"; 
+       document.getElementByld("load").style.visibility = "visible";  
 	}
 });
