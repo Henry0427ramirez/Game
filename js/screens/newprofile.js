bb1bd4875b48a5bc6ace15c566f4550664835ba6
Henry Ramirez <h.ramirez0427@gmail.com>
2015-04-03 09:00:39 -0700

    commit 62 error fix
    fixed my code from 53

--- js/screens/newprofile.js
+++ js/screens/newprofile.js
@@ -4,8 +4,8 @@ game.NewProfile = me.ScreenObject.extend({
 	 */
 	onResetEvent: function() {	
 		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('new-screen')), -10); // TODO
-        document.getElementByld("input").style.visibility = "visible";
-        document.getElementByld("register").style.visibility = "visible"; 
+        document.getElementById("input").style.visibility = "visible";
+        document.getElementById("register").style.visibility = "visible"; 
 
         me.input.unbindKey(me.input.KEY.B);
         me.input.unbindKey(me.input.KEY.Q);
