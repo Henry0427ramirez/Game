519066b20593b8c5011106574d6999ba9c8759b4
Henry Ramirez <h.ramirez0427@gmail.com>
2015-04-05 16:56:56 -0700

    commit 74
    added the minimap

--- js/entities/minimap.js
+++ js/entities/minimap.js
@@ -1,1 +1,17 @@
-minimap.js
+game.MiniMap = me.Entity.extend({{
+	init: function(x, y, settings){
+		this._super(me.Entity, "init", [x, y, {
+			image: "minimap" , 
+			width: 73,
+			height: 45,
+			spritewidth: "73",
+			spriteheight: "45",
+			getShape: function(){
+				return (new me.Rect(0 , 0, 73, 70)). polygon();
+			}
+		}]);
+   		this.floating = true;
+
+	}
+});
+
